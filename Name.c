8551227171b6f325cd89560c120db0acaf396bf5
GauravGurudutt <43572285+GauravGurudutt@users.noreply.github.com>
2018-09-25 17:54:30 +0530

    Create Name.c

---
+++ Name.c
@@ -0,0 +1,40 @@
+Write a program to print Hello World.
+
+#include<stdio.h>
+int main()
+{
+printf("Hello World.");
+return 0;
+}
+
+Write a program to print your name.
+
+#include<stdio.h>
+int main()
+{
+printf("My name is Gaurav G.");
+return 0;
+}
+
+Questions:
+
+What is #include<stdio.h>?
+
+#include<> is a preprocessor directive which includes the library stdio.h,which consists of standard input/output operations like printf,scanf,etc.
+
+What is printf?
+
+printf() is used to print any value(for ex. char,int,float,etc) onto the screen.
+
+What is main?
+
+main() is a function used to indicate the start of any C program;sometimes also indicating the execution of the program. 
+
+Why should you learn touch typing on QWERTY keyboard?
+
+To find it easy while writing the code,as it saves time by taking the values from the user faster and efficiently.
+
+How will you learn touch typing on a QWERTY keyboard?
+
+Touch typing on a QWERTY keyboard basically means using all ten fingers to type. Each key on the QWERTY keyboard is associated with a specific finger. It might be difficult at the start to use all fingers, but as we spend more time practicing, it becomes easier and faster.
+
